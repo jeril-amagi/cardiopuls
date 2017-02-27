1d96ca8eb8d4f0a35a5d8d4f17c74179ef2b0e7e
Jeril Sebastian <Jeril Sebastian>
2017-02-27 23:02:42 +0530

    header fixed

--- header.php
+++ header.php
@@ -1,3 +1,37 @@
+    <?php
+      function echoActiveClass($tab)
+      {
+          $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
+
+          if ($current_file_name == 'index' && $tab == 'index') 
+          {
+            echo 'active';
+          }
+          elseif ($current_file_name == 'contact' && $tab == 'contact') 
+          {
+            echo 'active';
+          }
+          elseif ($current_file_name == 'applications' && $tab == 'applications') 
+          {
+            echo 'active';
+          }
+          elseif ( ($current_file_name == 'illustrative_results' ||
+            $current_file_name == 'publications' ||
+            $current_file_name == 'cardiopuls_innovator' ||
+            $current_file_name == 'faqs')
+            && $tab == 'resources') 
+          {
+            echo 'active';
+          }
+          elseif ( ($current_file_name == 'what_is_cardiopuls' ||
+            $current_file_name == 'flowchart')
+            && $tab == 'about_us') 
+          {
+            echo 'active';
+          }
+      }
+    ?>
+
     <!--Header Of Theme-->
     <header class="clearfix" id="headerstic">
       <div class="mcare-header-bg">
@@ -76,12 +110,17 @@
                       "submit">Search</button>
                     </form><span class="morphsearch-close"></span>
                   </div><!-- /morphsearch -->
+
+
+                  <!-- Big screen tabs -->
                   <div class="overlay disable-hover noscroll"></div>
                   <ul class="nav navbar-nav navbar-right title-text top-nav">
-                    <li class="active">
+
+                    <li class='<?php echoActiveClass("index"); ?>' >
                       <a href="index.php"><i class="ion-ios-home-outline"></i>Home</a>
                     </li>
-                    <li class="dropdown">
+
+                    <li class='dropdown <?php echoActiveClass("about_us"); ?>' >
                       <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                       <i class="ion-ios-person-outline"></i>About Us<b class="icon-angle-down"></b></a>
                       <ul class="dropdown-menu single-drop">
@@ -94,11 +133,11 @@
                       </ul>
                     </li>
 
-                    <li>
+                    <li class='<?php echoActiveClass("applications"); ?>' >
                       <a href="applications.php"><i class="ion-ios-paperplane-outline"></i>Applications</a>
                     </li>
 
-                    <li class="dropdown">
+                    <li class='dropdown <?php echoActiveClass("resources"); ?>' >
                       <a class="dropdown-toggle" data-toggle="dropdown" href=
                       "#"><i class="ion-ios-box-outline"></i>Resources</a>
                       <ul class="dropdown-menu single-drop">
@@ -117,10 +156,12 @@
                         </li>
                       </ul>
                     </li>
-                    <li>
+
+                    <li class='<?php echoActiveClass("contact"); ?>' >
                       <a href="contact.php"><i class=
                       "ion-ios-location-outline"></i>Contact</a>
                     </li>
+
                   </ul>
                 </div><!-- /.navbar-collapse -->
               </nav>
