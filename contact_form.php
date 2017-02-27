6cdfedf2a9607554c187808597733ff565f6dde9
Jeril Sebastian <Jeril Sebastian>
2017-02-27 20:17:30 +0530

    contact form validation

--- contact_form.php
+++ contact_form.php
@@ -4,14 +4,14 @@
                 <div class="mcare-field control-group">
                   <i class="ion-person"></i>
                   <input class="form-control" id="name" 
-                  name="Name" placeholder="Name" type="text">
+                  name="Name" placeholder="Name" type="text" required>
                 </div>
               </div><!-- Email Text Box -->
               <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 no-pad">
                 <div class="mcare-field control-group">
                   <i class="ion-ios-email-outline"></i> 
                   <input class="form-control" id="Email" name="email" placeholder="Email"
-                  type="email">
+                  type="email" required>
                 </div>
               </div><!-- Subject Text Box -->
               <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 no-pad">
@@ -22,7 +22,7 @@
               </div><!-- Message Text Box -->
               <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 no-pad">
                 <div class="mcare-field control-group">
-                  <textarea class="form-control tarea" id="message" name="Message" placeholder="Message"></textarea>
+                  <textarea class="form-control tarea" id="message" name="Message" placeholder="Message" required></textarea>
                 </div>
               </div><!-- Button -->
               <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 no-pad">
